e0fc4c2157848eb58cce3a27f544abdecf9ce07a
smithadam7 <adamu777@live.com>
2019-03-06 17:13:35 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,57 @@
 import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import random
+import requests
+import os
 
-client = discord.Client()
+client = commands.Bot(command_prefix=";")
+player_dict = dict()
+
 
 @client.event
 async def on_ready():
-    print("The bot is ready!")
-    await client.change_presence(game=discord.Game(name="Nigloo"))
+    print("System Online")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content == "Hello":
-        await client.send_message(message.channel, "World")
+
+@client.command(pass_context=True)
+async def play (ctx, url):
+    channel = ctx.message.author.voice_channel
+    await client.join_voice_channel(channel)
+    server = ctx.message.server
+    voice = client.voice_client_in(server)
+    player = await voice.create_ytdl_player(url)
+    player_dict[server.id] = player
+    await client.send_message(ctx.message.channel, "Playing `%s` now" % player.title)
+    player.start()
+
+
+@client.command(pass_context=True)
+async def stop(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.stop()
+    await client.send_message(ctx.message.channel, "Stopped `%s`" % player.title)
+    del player_dict[server.id]
+
+
+@client.command(pass_context=True)
+async def pause(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.pause()
+    await client.send_message(ctx.message.channel, "Paused `%s`" % player.title)
+
+
+@client.command(pass_context=True)
+async def resume(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.resume()
+    await client.send_message(ctx.message.channel, "Resumed `%s`" % player.title)
+
+
+
 
 client.run(str(os.environ.get('BOT_TOKEN')))
